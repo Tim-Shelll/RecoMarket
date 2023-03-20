a93091c89a064f10b7f848eb726afbdbe9134ce1
Tim-Shelll <tiim.shelll@gmail.com>
2023-03-20 15:31:59 +0300

    Добавлен Create

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 site = Flask(__name__)
 site.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+site.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(site)
 
 
 class Product(db.Model):
     idItem = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
-    Desc = db.Column(db.String(200), nullable=False)
-    Price = db.Column(db.Integer, nullable=False)
-    # Img = ссылка
+    desc = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
     
     
 class Order(db.Model):
@@ -29,12 +29,30 @@ class ItemsInOrder(db.Model):
 
 @site.route('/')
 def index():
-    return render_template('index.html')
+    items = Product.query.order_by(Product.price).all()
+    return render_template('index.html', data=items)
 
 
 @site.route('/checkout')
 def checkout():
     return render_template('checkout.html')
+
+
+@site.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Product(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except: "Error"
+    else:
+        return render_template('create.html')
 
 
 if __name__ == "__main__":
