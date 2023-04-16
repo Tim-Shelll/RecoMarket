3442daff570578015f31ad4a37fcfa09f1b629a8
AndreyOvsyanov <andrey01109@gmail.com>
2023-04-16 14:34:16 +0300

    Task 1. Design

--- static/script/itemInBag.js
+++ static/script/itemInBag.js
@@ -19,3 +19,64 @@ document.addEventListener('DOMContentLoader', () => {
 
 })
 */
+
+const searchFocus = document.getElementById('search-focus');
+const keys = [
+  { keyCode: 'AltLeft', isTriggered: false },
+  { keyCode: 'ControlLeft', isTriggered: false },
+];
+
+window.addEventListener('keydown', (e) => {
+  keys.forEach((obj) => {
+    if (obj.keyCode === e.code) {
+      obj.isTriggered = true;
+    }
+  });
+
+  const shortcutTriggered = keys.filter((obj) => obj.isTriggered).length === keys.length;
+
+  if (shortcutTriggered) {
+    searchFocus.focus();
+  }
+});
+
+window.addEventListener('keyup', (e) => {
+  keys.forEach((obj) => {
+    if (obj.keyCode === e.code) {
+      obj.isTriggered = false;
+    }
+  });
+});
+
+let storage = document.getElementById('album py-5 bg-light')
+let container = storage.childNodes[1].childNodes[1].childNodes
+
+let search = document.getElementById('search-focus')
+
+function searchItItems() {
+    if (search.value == "") {
+        onload()
+        return;
+    }
+
+    container.forEach(function(element) {
+        if (element.tagName == 'DIV') {
+            let text = element.childNodes[1].childNodes[3].childNodes[1].textContent
+
+            if (text.toLowerCase().indexOf(search.value.trim().toLowerCase()) > -1) {
+                element.style.display = ''
+            } else {
+                element.style.display = 'none'
+            }
+
+        }
+    });
+}
+
+function onload() {
+    container.forEach(function(element) {
+        if (element.tagName == 'DIV') {
+             element.style.display = ''
+        }
+    })
+}
