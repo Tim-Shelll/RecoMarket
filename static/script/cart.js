3421848b3f4932f9aaaba49750c9066e022e0e14
AndreyOvsyanov <andrey01109@gmail.com>
2023-05-17 22:50:32 +0300

    Task 1. Корзина пользователя

--- static/script/cart.js
+++ static/script/cart.js
@@ -1,3 +1,7 @@
+window.onload = function () {
+    setAmount()
+}
+
 function actionItem(idItem, change, price) {
     $.ajax({
         type: 'POST',
@@ -5,8 +9,21 @@ function actionItem(idItem, change, price) {
         data: {'change': change},
         success: function(response) {
             $('#' + idItem).text(response[idItem]);
-            $('#' + idItem + '-price').text(response[idItem] * Number(price) + ' руб.')
+            $('#' + idItem + '-price').text(response[idItem] * Number(price) + ' руб');
+            setAmount()
         },
         error: function(error) {}
     })
+}
+
+function setAmount() {
+    let sum = 0
+    for (let id = 1; id <= 25; id++) {
+        let block = $('#' + id + '-price')
+        if (block.length != 0) {
+            sum += Number(block.text().split(' ')[0])
+        }
+    }
+
+    $('#amount').text(`Оплатить` + (sum == 0 ? `` : `: ${sum} руб`))
 }
