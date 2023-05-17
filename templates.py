d40ba0868265a320ce02d6e8366c07b627f39bbc
AndreyOvsyanov <andrey01109@gmail.com>
2023-05-17 23:26:25 +0300

    Task 1. Корзина пользователя

--- templates.py
+++ templates.py
@@ -1,6 +1,6 @@
 ORDERS_TO_USER = """
     SELECT * FROM (
-        SELECT O.client, i_i_o.idOrder, i_i_o.idItem, P.title, O.date, P.price, P.img  
+        SELECT O.client, i_i_o.idOrder, i_i_o.idItem, P.title, O.date, P.price, P.img, i_i_o.numItems  
         FROM items_in_order i_i_o
         LEFT JOIN 'order' as O ON O.idOrder = i_i_o.idOrder
         INNER JOIN Product as P ON p.idItem = i_i_o.idItem
