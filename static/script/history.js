c6f5f0af7f76101dacf6d011b88a5140b29011af
AndreyOvsyanov <andrey01109@gmail.com>
2023-05-25 15:32:18 +0300

    Modife

--- static/script/history.js
+++
@@ -1,7 +0,0 @@
-function onClickHistory(object) {
-    object.parentNode.childNodes.forEach(function(node) {
-        if (node.tagName == 'DIV') {
-            node.style.display = (node.style.display != 'none') ? 'none' : ''
-        }
-    })
-}
